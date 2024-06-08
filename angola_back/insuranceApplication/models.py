3c1d201c03e5254cab6cfb388993158be8f941bd
akobyansamvel <s.o.akobian@stud.mospolytech.ru>
2024-06-08 19:12:53 +0300

    passport & insurance app fix

--- angola_back/insuranceApplication/models.py
+++ angola_back/insuranceApplication/models.py
@@ -17,6 +17,12 @@ class InsApp(models.Model):
         ('подтверждено', 'Подтверждено'),
         ('отменено', 'Отменено'),
     ]
+    COMPANY_CHOICES = [
+        ('страховая 1','ООО "Nossa Seguros"'),
+        ('страховая 2','ООО "Seguríssimos"'),
+        ('страховая 3','ООО "Ensa"'),
+    ]
+
 
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=1)
     application_type = models.CharField(max_length=50, choices=APPLICATION_TYPE_CHOICES)
@@ -25,10 +31,11 @@ class InsApp(models.Model):
     passport_scan = models.ImageField(upload_to='passport_scans/', verbose_name='Скан паспорта', default=None, blank=False, null=True)
     signature_scan = models.ImageField(upload_to='signature_scans/', verbose_name='Скан подписи',default=None, blank=False, null=True)
     recipient = models.CharField(max_length=50, choices=RECIPIENT_CHOICES, verbose_name='Кому', default='себе')
+    company = models.CharField(max_length=50, choices= COMPANY_CHOICES, verbose_name='страховая', default='ООО "Nossa Seguros"')
     status = models.CharField(verbose_name='Статус', max_length=15, choices=STATUS_CHOICES, default='ожидание')
 
     def __str__(self):
-        return f"Заявка {self.user.email} на {self.application_type}"  # Изменили строковое представление модели
+        return f"Заявка {self.user.email} на {self.application_type}"  
 
     def get_passport_info(self):
-        return f"Паспорт: {self.passport_data.series_number} {self.passport_data.last_name} {self.passport_data.first_name}"  # Метод для получения информации о паспорте
+        return f"Паспорт: {self.passport_data.series_number} {self.passport_data.last_name} {self.passport_data.first_name}"  
--- angola_back/passportApplication/models.py
+++ angola_back/passportApplication/models.py
@@ -5,13 +5,15 @@ from passport.models import Passport
 
 class PasApp(models.Model): 
     APPLICATION_TYPE_CHOICES = [
-        ('получение', 'Получение'),
-        ('замена', 'Замена'),
+        ('получение', 'Получение паспорта'),
+        ('замена', 'Замена паспорта'),
     ]
-
-    RECIPIENT_CHOICES = [
-        ('себе', 'Себе'),
-        ('ребенку', 'Ребенку'),
+    CAUSE_CHOICES = [
+        ('возраст','Достижение 20 или 45 лет'),
+        ('фио','Изменение фамилии,имени или отчества'),
+        ('обстоятельства','Потеря или непригодность к использованию'),
+        ('ошибка','Ошибка в паспорте'),
+        ('внешность','Изменение внешности'),
     ]
 
     STATUS_CHOICES = [
@@ -22,11 +24,12 @@ class PasApp(models.Model):
 
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=1)
     application_type = models.CharField(max_length=50, choices=APPLICATION_TYPE_CHOICES)
-    reason = models.CharField(max_length=255)
-    passport_data = models.OneToOneField(Passport, on_delete=models.CASCADE, related_name='passport_application', default=1)  
+    passport_data = models.OneToOneField(Passport, on_delete=models.CASCADE, related_name='passport_application', default=1)
+    cause = models.CharField(verbose_name='причина', max_length=50, choices=CAUSE_CHOICES, default='Достижение 20 или 45 лет')
     passport_scan = models.ImageField(upload_to='passport_scans/', verbose_name='Скан паспорта',default=None, blank=False, null=True)
     signature_scan = models.ImageField(upload_to='signature_scans/', verbose_name='Скан подписи', default=None, blank=False, null=True)
-    recipient = models.CharField(max_length=50, choices=RECIPIENT_CHOICES, verbose_name='Кому',default='Себе')
+    marriage_scan = models.ImageField(upload_to='marriage_scans/', verbose_name='Скан брака', default=None, blank=False, null=True)
+    birth_scan = models.ImageField(upload_to='birth_scans/', verbose_name='Скан свидетельства', default=None, blank=False, null=True)
     status = models.CharField(verbose_name='Статус', max_length=15, choices=STATUS_CHOICES, default='ожидание')
 
     def __str__(self):
